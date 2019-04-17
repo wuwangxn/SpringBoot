daf8dd8a389dc0de4e5343ffee23c6c4d48427a6
xning <xningboy@163.com>
2019-04-17 18:38:38 +0800

    Spring Boot 整合 RabbitMQ

--- RabbitMq/src/main/java/com/xning/demo/rabbitMQ/listener/TestListener.java
+++ RabbitMq/src/main/java/com/xning/demo/rabbitMQ/listener/TestListener.java
@@ -1,10 +1,8 @@
 package com.xning.demo.rabbitMQ.listener;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
